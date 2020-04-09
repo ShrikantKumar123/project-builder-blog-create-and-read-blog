dde014ef9e23d8998eb0a25d8c86b999fa535f26
unknown <shrikant2994anil@gmail.com>
2020-04-09 22:36:18 +0530

    FSJ0320A005

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -10,34 +10,46 @@ import utility.ConnectionManager;
 
 public class UserDAO implements UserDaoInterface {
 
-	public int signUp(User user) {
+	public int signUp(User user) throws Exception {
 		String INSERT_USERS_SQL = "INSERT INTO USERS(email, password)VALUES(?,?)";
 
 		int result = 0;
 		try
 		{
-			Connection connection = ConnectionManager.getConnection();
-			// Step 2:Create a statement using connection object
+			Connection connection = null;
+			try {
+				connection = ConnectionManager.getConnection();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+
 			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
 			preparedStatement.setString(1,user.getEmail());
 			preparedStatement.setString(2,user.getPassword());
 			System.out.println(preparedStatement);
-			// Step 3: Execute the query or update query
+
 			result = preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
 		return result;
 	}
-	
-	public boolean loginUser(User user) {
+
+	public boolean loginUser(User user) throws Exception {
 		boolean status = false;
 		try{
-			Connection connection = ConnectionManager.getConnection();
-		
-				// Step 2:Create a statement using connection object
-		PreparedStatement preparedStatement = connection.prepareStatement("select * from users where email = ? and password = ? ");
-		
+			Connection connection = null;
+			try {
+				connection = ConnectionManager.getConnection();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+
+
+			PreparedStatement preparedStatement = connection.prepareStatement("select * from users where email = ? and password = ? ");
+
 			preparedStatement.setString(1, user.getEmail());
 			preparedStatement.setString(2, user.getPassword());
 
@@ -46,7 +58,7 @@ public class UserDAO implements UserDaoInterface {
 			status = rs.next();
 
 		} catch (SQLException e) {
-			// process sql exception
+
 			System.out.println(e);
 		}
 		return status;
